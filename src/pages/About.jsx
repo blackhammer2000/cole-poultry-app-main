f6cc4adeb1537f2e187b10fb38411c17954202e0
wawerusamuel <waweruzamuel@gmail.com>
2023-05-21 11:10:26 +0300

    made the about section card into one row...

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import { GiLaurelCrown, GiTargetArrows, GiCrystalGrowth } from 'react-icons/gi';
-import { FaRegCommentAlt } from 'react-icons/fa';
-import AboutUsSection from './AboutUsSection';
-import CardComponent from './Card';
-import './css/About.css';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { GiLaurelCrown, GiTargetArrows, GiCrystalGrowth } from "react-icons/gi";
+import { FaRegCommentAlt } from "react-icons/fa";
+import AboutUsSection from "./AboutUsSection";
+import CardComponent from "./Card";
+import "./css/About.css";
 const About = () => {
   return (
     <div>
       <AboutUsSection />
       <Container>
         <Row>
-          <Col md={6}>
+          <Col md={4} className="border-right border-danger">
             <CardComponent
               icon={<GiLaurelCrown className="about-us-icon" />}
               title="Vision"
               text="To be a globally recognized poultry management app that digitalizes processes in the poultry value chain."
             />
           </Col>
-          <Col md={6}>
+          <Col md={4}>
             <CardComponent
               icon={<GiTargetArrows className="about-us-icon" />}
               title="Mission"
@@ -26,7 +26,7 @@ const About = () => {
               "
             />
           </Col>
-          <Col md={6}>
+          <Col md={4}>
             <CardComponent
               icon={<GiCrystalGrowth className="about-us-icon" />}
               title="Core-Values"
--- src/pages/AboutUsSection.jsx
+++ src/pages/AboutUsSection.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import CompanyLogo from '../assets/logo.png';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import './css/AboutUsSection.css';
+import React, { useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import CompanyLogo from "../assets/logo.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./css/AboutUsSection.css";
 
 const AboutUsSection = () => {
   useEffect(() => {
@@ -17,22 +17,35 @@ const AboutUsSection = () => {
           <Col md={6} order={1} data-aos="fade-up">
             <div className="about-us-subsection">
               <div>
-                <h2 className="about-us-title fw-bold">Who We Are</h2>
-                <h1 className="about-us-subtitle">e-Poultry</h1>
+                <h2 className="about-us-title fw-bold">Who Are We</h2>
+                {/* <h1 className="about-us-subtitle">e-Poultry</h1> */}
+                <h4 className="hero-title">
+                  <span className="hero-highlight-red font-weight-bold big-e">
+                    e
+                  </span>
+                  -POULTRY
+                </h4>
                 <p>
-                We are an Agri fintech company on a mission to revolutionize poultry farming by leveraging on technology to offer robust solutions to the challenges facing value chain players and actors in poultry farming. 
-                Through the mobile based application, farmers are able to optimize their operations, increase profitability and track overall production of their flock.
-
-
+                  We are an Agri fintech company on a mission to revolutionize
+                  poultry farming by leveraging on technology to offer robust
+                  solutions to the challenges facing value chain players and
+                  actors in poultry farming. Through the mobile based
+                  application, farmers are able to optimize their operations,
+                  increase profitability and track overall production of their
+                  flock.
                 </p>
                 <a href="/product" className="ceo-button btn btn-primary">
-                OUR PRODUCTS
-               </a>
+                  OUR PRODUCTS
+                </a>
               </div>
             </div>
           </Col>
           <Col md={6} order={2} data-aos="fade-up">
-            <img src={CompanyLogo} alt="Company Logo" className="about-us-logo" />
+            <img
+              src={CompanyLogo}
+              alt="Company Logo"
+              className="about-us-logo"
+            />
           </Col>
         </Row>
       </Container>
